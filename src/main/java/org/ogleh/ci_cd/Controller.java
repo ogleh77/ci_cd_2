15023ea88cea4d11c03ebc2695afe72571fe97c3
Mohamed Ogleh <mohamedogleh0@gmail.com>
2024-08-21 10:43:20 +0300

    first commit

--- src/main/java/org/ogleh/ci_cd/Controller.java
+++ src/main/java/org/ogleh/ci_cd/Controller.java
@@ -1,13 +1,19 @@
 package org.ogleh.ci_cd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class Controller {
     @GetMapping("/greet")
     public String greet() {
         return "ASC World";
+    }
+
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable("name") String name) {
+        return "ASC " + name;
     }
 }
